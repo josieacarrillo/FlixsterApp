6551113cfef42f6e732f1f149e66af44a182911d
Josie Angelica Carrillo <josieacarrillo@gmail.com>
2020-03-28 20:24:23 -0700

    fetching data properly

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String TAG = "MainActivity";
 
-    List<Movie> movies;
+    List<Movie> movies; //member variable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
                     JSONArray results =jsonObject.getJSONArray("results");
                     Log.i(TAG, "Results:"+ results.toString());
                     movies = Movie.fromJsonArray(results);
+                    Log.i(TAG, "Movies:"+ movies.size());
                 } catch (JSONException e) {
                     Log.e(TAG,"Hit json exception", e);
                     e.printStackTrace();
--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -12,17 +12,35 @@ public class Movie {
     String title;
     String overview;
 
+    //construct movie objects
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
     }
 
+    //take jsonArray and turn it into an array of movies
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
         List<Movie> movies = new ArrayList<>();
         for (int i = 0; i < movieJsonArray.length(); i++) {
             movies.add(new Movie(movieJsonArray.getJSONObject(i)));
         }
         return movies;
+    }
+
+    //get data out of the movie objects
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    //hardcoding sizes
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s",posterPath);
+    }
+
+    public String getOverview() {
+        return overview;
     }
 }
